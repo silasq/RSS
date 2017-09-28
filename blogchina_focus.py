5e638fed20b0bbe2be6b207fd490a19f1c67b0bf
silasq <30379895+silasq@users.noreply.github.com>
2017-09-28 22:21:28 +0800

    add

---
+++ blogchina_focus.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from RssSpider import RssSpider
+
+if __name__ == '__main__':
+    rssSpider = RssSpider(title='博客中国-聚焦',
+                          link=r'http://www.blogchina.com/',
+                          xmlpath=r'/var/www/html/blogchina_focus.xml',
+                          baseurl=r"http://tuijian.blogchina.com/list/index/flag/2/channel/2")
+    rssSpider.search_soup('div', {'class': 'news_toplist_left'})
+    rssSpider.get_links('li', {'class': 'tit_a'}, 'a')
+    rssSpider.enterpage('div',{'class': 'article'})
+    rssSpider.SaveRssFile()
