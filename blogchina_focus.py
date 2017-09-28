ee954bcc6cec888f0ebf620742d3fa13f4f27fc8
silasq <30379895+silasq@users.noreply.github.com>
2017-09-28 22:25:38 +0800

    update

--- blogchina_focus.py
+++ blogchina_focus.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
                           xmlpath=r'/var/www/html/blogchina_focus.xml',
                           baseurl=r"http://tuijian.blogchina.com/list/index/flag/2/channel/2")
     rssSpider.search_soup('div', {'class': 'news_toplist_left'})
-    rssSpider.get_links('li', {'class': 'tit_a'}, 'a')
+    rssSpider.get_links('li', {'class': 'tit'}, 'a')
     rssSpider.enterpage('div',{'class': 'article'})
     rssSpider.SaveRssFile()
