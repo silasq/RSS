2fce0cf0304815dd775e34e7d940338961e11a8b
silasq <30379895+silasq@users.noreply.github.com>
2017-10-01 20:37:18 +0800

    commit

--- jiandan_new.py
+++ jiandan_new.py
@@ -11,4 +11,5 @@ if __name__ == '__main__':
     rssSpider.get_links('div', {'class': 'thumbs_b'}, 'a')
     rssSpider.enterpage('div', {'id': 'content'})
     rssSpider.replace('data-original="','src="http:')
+    rssSpider.replace('alt=".*"', '')
     rssSpider.SaveRssFile()
